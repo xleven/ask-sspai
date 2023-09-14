870f1df3d78eb47a57bc393a2d4ead9db3472ede
xleven <xleven@outlook.com>
2023-09-14 13:34:23 +0800

    fix: input hints

--- components/prompt-form.tsx
+++ components/prompt-form.tsx
@@ -69,7 +69,7 @@ export function PromptForm({
           rows={1}
           value={input}
           onChange={e => setInput(e.target.value)}
-          placeholder="Send a message."
+          placeholder="Ask a question."
           spellCheck={false}
           className="min-h-[60px] w-full resize-none bg-transparent px-4 py-[1.3rem] focus-within:outline-none sm:text-sm"
         />
@@ -82,10 +82,10 @@ export function PromptForm({
                 disabled={isLoading || input === ''}
               >
                 <IconArrowElbow />
-                <span className="sr-only">Send message</span>
+                <span className="sr-only">Ask question</span>
               </Button>
             </TooltipTrigger>
-            <TooltipContent>Send message</TooltipContent>
+            <TooltipContent>Ask question</TooltipContent>
           </Tooltip>
         </div>
       </div>
